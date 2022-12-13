5d43ad784653074236878eca1fe28dd350908d27
“vipul-ksolves” <vipul.patel@ksolves.com>
2022-12-13 11:40:21 +0530

    variables

---
+++ react-in-typeScript/practicle/variable.js
@@ -0,0 +1,2 @@
+var myName = "Hellow Vipul";
+console.log(myName);
---
+++ react-in-typeScript/practicle/variable.ts
@@ -0,0 +1,17 @@
+//string
+let myName: string = "Hellow Vipul";
+console.log(myName);
+
+//number
+let userID: number = 3828282;
+
+// boolean
+let isLogedIn: boolean = false;
+
+//any
+let anyVariable;
+function anyValue() {
+  return "Any Varibale";
+}
+
+export {};
